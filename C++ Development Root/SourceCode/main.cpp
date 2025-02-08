e1c969b2426dbf1dc3dc970707d717cef4b3f234
mia labree <mialabree@csu.fullerton.edu>
2025-02-08 23:38:44 +0000

    main

--- C++ Development Root/SourceCode/main.cpp
+++ C++ Development Root/SourceCode/main.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
-  std::cout << "C++ Rocks!\n";
+  std::cout << "Welcome to Kroger.  Place grocery items into your shopping cart by entering each item's information." << std::endl;
+  std::cout << "enclose string entries in quotes, separate fields with comas" << std::endl;
+  std::cout << "for example:  '00016000306707', 'Betty Crocker', 'Betty Crocker Double Chocolate Chunk Cookie Mix', 17.19" << std::endl;
+  std::cout << "Enter CTL-Z (Windows) or CTL-D (Linux) to quit" << std::endl;
 }
