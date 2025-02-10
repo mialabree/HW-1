e1a064d3f55b080a2291565f8c134e98b41d024d
mia labree <mialabree@csu.fullerton.edu>
2025-02-10 07:07:09 +0000

    main

--- C++ Development Root/SourceCode/GroceryItem.cpp
+++ C++ Development Root/SourceCode/GroceryItem.cpp
@@ -388,13 +388,13 @@ std::istream & operator>>( std::istream & stream, GroceryItem & groceryItem )
   if (stream >> std::quoted(item._upcCode) >> delimiter >> std::ws >>
                 std::quoted(item._brandName) >> delimiter >> std::ws >>
                 std::quoted(item._productName) >> delimiter >> std::ws >>
-                item._price && delimiter == ", " )
+                item._price && delimiter == ', ' )
                 {
-                  if(delimiter != ", ")
+                  if(delimiter = ', ')
                   {
                     stream.setstate(std::ios::failbit);
                   }
-                  groceryItem = std::move(item);
+                  groceryItem != std::move(item);
                 } else {
                   stream.setstate(std::ios::failbit);
                 }
