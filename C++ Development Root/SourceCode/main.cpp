dbc61118babbff1efd9aaae97a21a717796e15e2
mia labree <mialabree@csu.fullerton.edu>
2025-02-10 06:24:03 +0000

    new changes

--- C++ Development Root/SourceCode/main.cpp
+++ C++ Development Root/SourceCode/main.cpp
@@ -7,24 +7,22 @@ int main()
   std::cout << "for example:  '00016000306707', 'Betty Crocker', 'Betty Crocker Double Chocolate Chunk Cookie Mix', 17.19" << std::endl;
   std::cout << "Enter CTL-Z (Windows) or CTL-D (Linux) to quit" << std::endl;
 
-  std::vector<std::unique_ptr < GroceryItem >> itemList;
-
-  std::ifstream file("input.txt");
-
-  if (!file.is_open()) {
-    std::cout << "Error with file";
-    return 0;
-  }
-
+  std::vector<std::unique_ptr<GroceryItem>>itemList;
   std::string inputLine;
 
-  while(getline(file, inputLine))
+  while(std::cin >> inputLine)
   {
     std::cout << "Enter UPC, Product Brand, Product Name, and Price" << std::endl;
     auto newItem = std::make_unique <GroceryItem> (inputLine);
     itemList.push_back(std::move(newItem));
-    std::cout << "Item added to shopping cart:" << inputLine << std::endl;
+    std::cout << "Item added to shopping cart:" << inputLine << '\n';
   }
 
+  std::cout << "Here is an itemized list of the items in your shopping cart:\n"
+
+  for(std::vector<std::unique_ptr<GroceryItem>>::const_reverse_iterator i = shoppingCart.crbegin(); i != shoppingCart.crend(); ++i) {
+    std::cout << i << '\n';
+  }
+  return 0;
 }
 
