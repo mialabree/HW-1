6d491ea084934426426238f3ce6b339bfd6a7979
mia labree <mialabree@csu.fullerton.edu>
2025-02-10 07:16:42 +0000

    main

--- C++ Development Root/SourceCode/main.cpp
+++ C++ Development Root/SourceCode/main.cpp
@@ -9,17 +9,18 @@ int main()
   std::vector<std::unique_ptr<GroceryItem>> shoppingCart{};
   GroceryItem newProduct; 
 
-  std::cout << "Welcome to Kroger.  Place grocery items into your shopping cart by entering each item's information." << std::endl;
-  std::cout << "enclose string entries in quotes, separate fields with comas" << std::endl;
-  std::cout << "for example:  '00016000306707', 'Betty Crocker', 'Betty Crocker Double Chocolate Chunk Cookie Mix', 17.19" << std::endl;
-  std::cout << "Enter CTL-Z (Windows) or CTL-D (Linux) to quit" << std::endl;
+  std::cout << "Welcome to Kroger.  Place grocery items into your shopping cart by entering each item's information.\n" 
+            << "enclose string entries in quotes, separate fields with comas\n" 
+            << "for example:  '00016000306707', 'Betty Crocker', 'Betty Crocker Double Chocolate Chunk Cookie Mix', 17.19\n" 
+            << "Enter CTL-Z (Windows) or CTL-D (Linux) to quit \n\n";
 
+  std::cout << "Enter UPC, Product Brand, Product Name, and Price: ";
   while(std::cin >> newProduct)
     {
-        std::cout << "Enter UPC, Product Brand, Product Name, and Price\n";
         auto newItem = std::make_unique<GroceryItem>(newProduct);
         shoppingCart.push_back(std::move(newItem));
         std::cout << "Item added to shopping cart: " << newProduct << "\n\n";
+        std::cout << "Enter UPC, Product Brand, Product Name, and Price: ";
     }
 
     std::cout << "Here is an itemized list of the items in your shopping cart:\n";
